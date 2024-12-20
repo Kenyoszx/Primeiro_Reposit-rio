9ceed697961d158fa7a18efdebcb8fc1ff0d272d
João Victor de Souza Silva <joao1452019@gmail.com>
2024-12-20 00:36:52 -0300

    Somador de 2 Numeros

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World");
+    int a=0,b=0,c=0;
+    printf("\nEscreva o primeiro Numero (a): ");
+    scanf("%d",&a);
+    printf("\nEscreva o segundo Numero (b): ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("\nO Resultado da soma entre a e b vale: %d",c);
     return 0;
 }
